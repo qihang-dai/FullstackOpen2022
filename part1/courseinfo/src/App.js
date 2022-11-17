1aaf31958a7e5025d63ff4f86ea25e70b8f3d55b
Lee Tai <553269487@qq.com>
2022-11-17 00:48:40 -0800

    part a

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,15 @@
+const
+
+const App = () => {
+  // const-definitions
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content ... />
+      <Total ... />
+    </div>
+  )
+}
+
+export default App
